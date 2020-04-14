5e05551e007f7f46324ac440b43501681aa8b130
Lambda Moses <dlu2@caltech.edu>
2020-04-13 19:12:40 -0700

    Only use uwot for UMAP

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,7 @@
 #' Tools for single-cell genomics
+#' 
+#' This is a slimmed down version of Seurat, only keeping functionalities deemed
+#' the most commonly used.
 #'
 #' @section Package options:
 #'
@@ -9,23 +12,20 @@
 #'   This can be helpful in cleaning up the memory status of the R session and
 #'   prevent use of swap space. However, it does add to the computational overhead
 #'   and setting to FALSE can speed things up if you're working in an environment
-#'   where RAM availabiliy is not a concern.}
-#'   \item{\code{Seurat.warn.umap.uwot}}{Show warning about the default backend
-#'   for \code{\link{RunUMAP}} changing from Python UMAP via reticulate to UWOT}
+#'   where RAM availability is not a concern.}
 #'   \item{\code{Seurat.checkdots}}{For functions that have ... as a parameter,
 #'   this controls the behavior when an item isn't used. Can be one of warn,
 #'   stop, or silent.}
 #' }
 #'
 #' @docType package
-#' @rdname Seurat-package
-#' @name Seurat-package
+#' @rdname SeuratBasics-package
+#' @name SeuratBasics-package
 #'
 NULL
 
 seurat_default_options <- list(
   Seurat.memsafe = FALSE,
-  Seurat.warn.umap.uwot = TRUE,
   Seurat.checkdots = "warn"
 )
 
